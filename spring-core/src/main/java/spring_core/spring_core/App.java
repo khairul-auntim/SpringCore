d23b27f0750217d244674ed2db4a91c91c9ec079
Mosaddik <mosaddikbinhafiz@gmail.com>
2021-09-17 12:44:52 +0200

    Modified

--- spring-core/src/main/java/spring_core/spring_core/App.java
+++ spring-core/src/main/java/spring_core/spring_core/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Git Integration Testing");
+        System.out.println("Git Integration Testing V2");
     }
 }
