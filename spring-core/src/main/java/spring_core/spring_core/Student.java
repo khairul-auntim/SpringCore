f97807b0f3376762f54f9ba6b2e652e3c2836928
antim <khairul.auntim@gmail.com>
2021-09-17 12:57:36 +0200

    Student and Config File Are Added

---
+++ spring-core/src/main/java/spring_core/spring_core/Student.java
@@ -0,0 +1,15 @@
+package spring_core.spring_core;
+
+public class Student {
+	private int studentID;
+
+	public int getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	
+
+}
